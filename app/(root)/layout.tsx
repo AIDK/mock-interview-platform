3576f0c1a82604f300956eec9b2d87c6cf2be8c4
AndriesDK <andriesd@cashcrusaders.co.za>
2025-03-28 10:28:25 +0200

    implemented home page with interview layouts

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,6 +1,19 @@
 ﻿import { ReactNode } from "react";
+import Link from "next/link";
+import Image from "next/image";
 
 const RootLayout = ({ children }: { children: ReactNode }) => {
-  return <div>{children}</div>;
+  return (
+    <div className={"root-layout"}>
+      <nav>
+        <Link href={"/"} className={"flex item-center gap-2"}>
+          <Image src={"/logo.svg"} alt={"Logo"} width={38} height={32} />
+          <h2 className={"text-primary-100"}>Preppi</h2>
+        </Link>
+      </nav>
+
+      {children}
+    </div>
+  );
 };
 export default RootLayout;
