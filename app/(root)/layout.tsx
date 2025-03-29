0cf4744edf18a5a9f45f58d1b8d236e39693bfa1
Andries de Kock <andries.de@gmail.com>
2025-03-29 13:12:58 +0200

    implemented authenticated routing

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,8 +1,14 @@
 ﻿import { ReactNode } from "react";
 import Link from "next/link";
 import Image from "next/image";
+import { isAuthenticated } from "@/lib/actions/auth.action";
+import { redirect } from "next/navigation";
 
-const RootLayout = ({ children }: { children: ReactNode }) => {
+const RootLayout = async ({ children }: { children: ReactNode }) => {
+  // we check for logged-in user and redirect to sign-in if not found
+  const isUserAuthenticate = await isAuthenticated();
+  if (!isUserAuthenticate) redirect("/sign-in");
+
   return (
     <div className={"root-layout"}>
       <nav>
