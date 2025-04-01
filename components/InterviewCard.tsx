da5bd73fc334535b0975eb5cbd7032659a95fb9b
AndriesDK <andriesd@cashcrusaders.co.za>
2025-04-01 13:47:39 +0200

    implemented interview feedback

--- components/InterviewCard.tsx
+++ components/InterviewCard.tsx
@@ -4,8 +4,9 @@ import { getRandomInterviewCover } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import DisplayTechIcons from "@/components/DisplayTechIcons";
+import { getFeedbackByInterviewId } from "@/lib/actions/general.action";
 
-const InterviewCard = ({
+const InterviewCard = async ({
   id,
   userId,
   role,
@@ -13,9 +14,12 @@ const InterviewCard = ({
   techstack,
   createdAt,
 }: InterviewCardProps) => {
-  const feedback = null as Feedback | null;
-  const normalizedType = /mix/gi.test(type) ? "Mixed" : type;
+  const feedback =
+    id && userId
+      ? await getFeedbackByInterviewId({ interviewId: id, userId })
+      : null;
 
+  const normalizedType = /mix/gi.test(type) ? "Mixed" : type;
   const formattedDate = dayjs(
     feedback?.createdAt || createdAt || Date.now(),
   ).format("MMM D, YYYY");
