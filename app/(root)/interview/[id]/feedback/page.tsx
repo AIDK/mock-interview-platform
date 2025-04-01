e169d9321e36e72c157fe0c77a017dae076699e9
AndriesDK <andriesd@cashcrusaders.co.za>
2025-04-01 11:02:57 +0200

    implement interview feedback using AI scoring of interview transcript

---
+++ app/(root)/interview/[id]/feedback/page.tsx
@@ -0,0 +1,24 @@
+﻿import { getCurrentUser } from "@/lib/actions/auth.action";
+import {
+  getFeedbackByInterviewId,
+  getInterviewById,
+} from "@/lib/actions/general.action";
+import { redirect } from "next/navigation";
+
+const Page = async ({ params }: RouteParams) => {
+  const { id } = await params;
+  const interview = await getInterviewById(id);
+
+  if (!interview) redirect("/");
+  const user = await getCurrentUser();
+
+  const feedback = await getFeedbackByInterviewId({
+    interviewId: id,
+    userId: user?.id!,
+  });
+
+  console.log(feedback);
+
+  return <div>Page</div>;
+};
+export default Page;
